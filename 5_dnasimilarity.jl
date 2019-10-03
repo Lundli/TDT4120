1479b1671463eb44a56cde3442242965382d299a
lundli <lundli@pm.me>
2019-10-03 14:47:12 +0000

    Exercise 1 finished

--- 5_dnasimilarity.jl
+++ 5_dnasimilarity.jl
@@ -8,5 +8,12 @@
 
 # Exercise 1
 function dnasimilarity(s1, s2)
-
+    amount = 0
+    for i = 1:length(s1)
+        if (s1[i] == s2[i])
+            amount += 1
+        end
+    end
+    return amount
 end
+
