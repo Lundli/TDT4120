087c73b074a2c3f5b1489e4cd0ea31210df75aa4
lundli <lundli@pm.me>
2019-10-16 16:04:27 +0200

    started exercise 2

---
+++ 7_mincoincsgreedy.jl
@@ -0,0 +1,21 @@
+# Exercise 2
+function mincoinsgreedy(coins, value)
+    # Coins: a list of available coins
+    # Value: a value composed by as few coins as possible
+
+    usedCoins = 0
+    remaining = value
+
+
+    # for loop start
+    for i = 1:length(coins)
+    # todo
+    end
+
+
+    return usedCoins
+end
+
+
+mincoinsgreedy([1000,500,100,20,5,1],8)
+
