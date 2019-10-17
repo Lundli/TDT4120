786b560d84535f67002442e43600c4118a2b1d9a
lundli <lundli@pm.me>
2019-10-17 10:04:06 +0200

    exercise 2: tests added

--- 7_mincoincsgreedy.jl
+++ 7_mincoincsgreedy.jl
@@ -25,3 +25,16 @@ end
 mincoinsgreedy([1000,500,100,20,5,1],8)
 
 
+
+
+### Tests ###
+printstyled("\n\n\n---------------\nTests start\n---------------\n"; color = :magenta)
+
+using Test
+@testset "Tester" begin
+	@test mincoinsgreedy([1000,500,100,20,5,1],1226) == 6
+  @test mincoinsgreedy([20,10,5,1],99) == 10
+  @test mincoinsgreedy([5,1],133) == 29
+end
+
+println("---------------------------------------------------------\n\n")
