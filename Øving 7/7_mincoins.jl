267c09fa40682ec478c2588ffaf6ea912698416a
lundli <lundli@pm.me>
2019-10-17 11:57:28 +0200

    exercise 3: added tests

--- Øving 7/7_mincoins.jl
+++ Øving 7/7_mincoins.jl
@@ -1,7 +1,22 @@
 # Exercise 3
 
 function mincoins(coins, value)
-    # Om du ikke trenger inf kan du fjerne den
+
     inf = typemax(Int)
-    # Din kode her
+
+
 end
+
+
+
+
+using Test
+@testset "Tester" begin
+	@test mincoins([4,3,1],18) == 5
+  @test mincoins([1000,500,100,30,7,1],14) == 2
+  @test mincoins([40, 30, 20, 10, 1], 90) == 3
+end
+
+println("\nFungerte alt? Prøv å kjør koden i inginious!")
+println("Husk at disse testene ikke alltid sjekker alle edge-cases")
+println("---------------------------------------------------------\n\n")
