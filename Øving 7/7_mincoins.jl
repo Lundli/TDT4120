dc8bc4c6be52eb9709afd23151eb55978a8f6ef6
lundli <lundli@pm.me>
2019-10-17 12:06:15 +0200

    exercise 3: added tests

--- Øving 7/7_mincoins.jl
+++ Øving 7/7_mincoins.jl
@@ -1,10 +1,54 @@
+# Helper function from exercise 1
+function usegreed(coins)
+    # loop from second coin to last
+    flag = true
+
+    for i = 2:length(coins)
+        if ((coins[i-1] % coins[i]) != 0)
+            flag = false
+        end
+    end
+    return flag
+end
+#--------------------------------------------#
+
+
+
 # Exercise 3
-
 function mincoins(coins, value)
-
     inf = typemax(Int)
 
 
+	# If value is zero
+    if (value == 0)
+        return 0
+    end
+
+
+	# If greedy approach works
+	if (usegreed(coins))
+		minCoins = usegreed(coins)
+		return minCoins
+	end
+
+
+    # Try each coin lower than Value
+
+    for i = 1:length(coins)
+        if (coins[i] <= value)
+
+    		# recursive call
+            sub_res = mincoins(coins, value-coins[i])
+
+            # Check for INT_MAX to avoid overflow and see if
+            # result can minimized
+            if (sub_res != inf && sub_res + 1 < inf)
+                inf = sub_res + 1
+            end
+        end
+    end
+
+    return inf
 end
 
 
