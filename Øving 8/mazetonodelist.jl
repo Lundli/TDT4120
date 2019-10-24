a3aaa060c78c195671973eb6859a21b33b9f8488
lundli <lundli@pm.me>
2019-10-24 14:01:43 +0200

    exercise 1 finished

--- Øving 8/mazetonodelist.jl
+++ Øving 8/mazetonodelist.jl
@@ -22,10 +22,12 @@ maze = [0 0 0 0 0 0 0
 function mazetonodelist(maze)
     nodearray = Array{Node}(undef, size(maze, 1), size(maze, 2))
 
+    # Add all nodes to nodearray.
+    # with true if floor, with false if it's a wall
     for i in 1:size(maze, 1)
         for j in 1:size(maze, 2)
             if (maze[i,j] == 1)
-                nodearray[i, j] = Node(i, j)
+                nodearray[i, j] = Node(i, j, true)
             else
                 nodearray[i, j] = Node(i, j, false)
             end
@@ -34,32 +36,45 @@ function mazetonodelist(maze)
 
     for i in 1:size(maze, 1)
         for j in 1:size(maze, 2)
-            # Check if node is floor
+
+            # Check if node is floor (not a wall)
             if (nodearray[i,j].floor == true)
+                println("Hey")
 
-                # North (i-1)
+                # North, (i-1)
                 if (( (i-1) >= 1) && (nodearray[i-1,j].floor == true))
+                    push!(nodearray[i,j].neighbors, nodearray[i-1,j])
 
-                # South (i+1)
-                elseif (( (i+1) >= SOME_SIZE) && (nodearray[i+1,j].floor == true))
+                # South, (i+1)
+                elseif (( (i+1) <= size(maze,1)) && (nodearray[i+1,j].floor == true))
+                    push!(nodearray[i,j].neighbors, nodearray[i+1,j])
 
+                # East, (j+1)
+                elseif (( (j+1) <= size(maze, 2))   && (nodearray[i,j+1].floor == true))
+                    push!(nodearray[i,j].neighbors, nodearray[i,j+1])
 
-                # East (j+1)
-                elseif (( (i-1) >= SOME_SIZE)   && (nodearray[i,j+1].floor == true))
-
-                # West (j-1)
-                elseif (( (i-1) >= SOME_SIZE) && (nodearray[i,j-1].floor == true))
-
+                # West, (j-1)
+                elseif (( (j-1) >= 1) && (nodearray[i,j-1].floor == true))
+                    push!(nodearray[i,j].neighbors, nodearray[i,j-1])
 
                 end
-             #
-            #push!(nodearray[i,j].neighbors, nodearray[i-1,j])
             end
         end
     end
-    =#
-    # TODO: create nodelist from nodearray
-    return 100
+
+
+    nodeList = []
+    for i = 1:size(maze,1)
+        for j = 1:size(maze,2)
+
+            # Only add if node has value 1 (floor)
+            if nodearray[i,j].floor == true
+                push!(nodeList, nodearray[i,j])
+            end
+        end
+    end
+
+    return nodeList
 end
 
-println(mazetonodelist(maze))
+#println(mazetonodelist(maze))
