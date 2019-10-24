f48d064efdd3f7499a5946fc51675603d711be6b
lundli <lundli@pm.me>
2019-10-24 13:17:34 +0200

    add nodes to nodearray

--- Øving 8/mazetonodelist.jl
+++ Øving 8/mazetonodelist.jl
@@ -1,5 +1,4 @@
 # Exercise 1
-
 mutable struct Node
     i::Int
     j::Int
@@ -11,24 +10,56 @@ mutable struct Node
 end
 Node(i, j, floor=true) = Node(i, j, floor, [], nothing, nothing, nothing)
 
+
+maze = [0 0 0 0 0 0 0
+                   1 1 0 1 1 1 0
+                   0 1 0 1 0 0 0
+                   0 1 0 1 1 1 0
+                   0 1 1 1 0 1 0
+                   0 1 0 1 0 1 1
+                   0 0 0 0 0 0 0]
+
 function mazetonodelist(maze)
-    # Vi lager en matrise nodearray med størrelse tilsvarende maze,
-    # men med Node-objekter isteden
     nodearray = Array{Node}(undef, size(maze, 1), size(maze, 2))
 
     for i in 1:size(maze, 1)
         for j in 1:size(maze, 2)
-            # Fyll inn kode for å oppdatere nodearray
+            if (maze[i,j] == 1)
+                nodearray[i, j] = Node(i, j)
+            else
+                nodearray[i, j] = Node(i, j, false)
+            end
         end
     end
 
     for i in 1:size(maze, 1)
         for j in 1:size(maze, 2)
-            # Fyll inn kode for å oppdatere neighbors til hver node
-            # (Husk at naboene alltid er rett over, rett under,
-            #  rett til venstre og/eller rett til høyre)
+            # Check if node is floor
+            if (nodearray[i,j].floor == true)
+
+                # North (i-1)
+                if (( (i-1) >= 1) && (nodearray[i-1,j].floor == true))
+
+                # South (i+1)
+                elseif (( (i+1) >= SOME_SIZE) && (nodearray[i+1,j].floor == true))
+
+
+                # East (j+1)
+                elseif (( (i-1) >= SOME_SIZE)   && (nodearray[i,j+1].floor == true))
+
+                # West (j-1)
+                elseif (( (i-1) >= SOME_SIZE) && (nodearray[i,j-1].floor == true))
+
+
+                end
+             #
+            #push!(nodearray[i,j].neighbors, nodearray[i-1,j])
+            end
         end
     end
-
-    # Fyll inn kode for å returnere en nodeliste ut ifra nodearray
+    =#
+    # TODO: create nodelist from nodearray
+    return 100
 end
+
+println(mazetonodelist(maze))
