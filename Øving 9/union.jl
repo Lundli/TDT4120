bb35c221de43cd1cc2724b5e8c136fbeae7c0ad6
lundli <lundli@pm.me>
2019-11-05 15:25:32 +0100

    exercise 2 finished

---
+++ Øving 9/union.jl
@@ -0,0 +1,25 @@
+function findset(x)
+    if x != x.p
+        x.p = findset(x.p)
+    end
+    return x.p
+end
+
+# Exercise 2
+function link(x, y)
+    if (x.rank > y.rank)
+        y.p = x
+    else
+        x.p = y
+        if (x.rank == y.rank)
+            y.rank = y.rank + 1
+        end
+    end
+end
+
+
+function union!(x, y)
+    link(findset(x), findset(y))
+end
+
+
